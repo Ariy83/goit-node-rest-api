ee4c89120e75e163c198ded376aa318ac8b44744
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2024-02-20 20:26:14 +0200

    fix_login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -15,22 +15,23 @@ const register = async (req, res) => {
   }
 
   const newUser = await signup(req.body);
+
   res.status(201).json({
     user: { email: newUser.email, subscription: newUser.subscription },
   });
 };
 
 const login = async (req, res) => {
-  const { email, password, subscription } = req.body;
+  const { email, password } = req.body;
 
   const user = await findUser({ email });
   if (!user) {
-    throw HttpError(401, "Email is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
 
-  const passwordCompare = bcrypt.compare(password, user.password);
+  const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw HttpError(401, "Password is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
 
   const payload = {
@@ -39,7 +40,7 @@ const login = async (req, res) => {
 
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
 
-  res.json({ token, user: email, subscription });
+  res.json({ token, user: { email, subscription: user.subscription } });
 };
 
 export default {
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -4,11 +4,12 @@ import { emailRegexp } from "../constants/regexp.js";
 export const signupSchema = Joi.object({
   password: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  subscription: Joi.string().valid("starter", "pro", "business").required(),
+  subscription: Joi.string().valid("starter", "pro", "business"),
 });
 
 export const signinSchema = Joi.object({
   password: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
+  subscription: Joi.string().valid("starter", "pro", "business"),
   token: [Joi.string(), Joi.number()],
 });
